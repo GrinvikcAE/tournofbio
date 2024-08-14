9c8b307a898356ea6d17d6f72fff68c5afb623be
GRAE <grinvikc@gmail.com>
2024-08-15 00:33:38 +0700

    fixes

--- main.py
+++ main.py
@@ -35,25 +35,25 @@ admin = Admin(app=app, engine=async_session_maker, authentication_backend=auth_b
 
 @app.on_event("startup")
 async def add_on_startup():
-    try:
-        admin.add_view(RoleAdmin)
-        admin.add_view(UserAdmin)
-        admin.add_view(CommandAdmin)
-        admin.add_view(MemberAdmin)
-        async with engine.begin() as conn:
-            roles = [{'id': 1, 'name': 'super-admin', 'permissions': []},
-                     {'id': 2, 'name': 'admin', 'permissions': []},
-                     {'id': 3, 'name': 'master', 'permissions': []},
-                     {'id': 4, 'name': 'jury', 'permissions': []},
-                     {'id': 5, 'name': 'command', 'permissions': []},
-                     {'id': 6, 'name': 'nobody', 'permissions': []}]
-            try:
-                await conn.execute(role.insert(), roles)
-            except Exception as e:
-                print(e)
-        await engine.dispose()
-    except Exception as e:
-        print(e)
+    # try:
+    #     admin.add_view(RoleAdmin)
+    #     admin.add_view(UserAdmin)
+    #     admin.add_view(CommandAdmin)
+    #     admin.add_view(MemberAdmin)
+    #     async with engine.begin() as conn:
+    #         roles = [{'id': 1, 'name': 'super-admin', 'permissions': []},
+    #                  {'id': 2, 'name': 'admin', 'permissions': []},
+    #                  {'id': 3, 'name': 'master', 'permissions': []},
+    #                  {'id': 4, 'name': 'jury', 'permissions': []},
+    #                  {'id': 5, 'name': 'command', 'permissions': []},
+    #                  {'id': 6, 'name': 'nobody', 'permissions': []}]
+    #         try:
+    #             await conn.execute(role.insert(), roles)
+    #         except Exception as e:
+    #             print(e)
+    #     await engine.dispose()
+    # except Exception as e:
+    #     print(e)
     try:
         users = [
             {'email': ADMIN,
